053302c0d954b041e68c0285d9411799549d93be
Pavel Khokhlov <ya.pavel-kh@yandex.ru>
2020-12-23 22:49:27 +0300

    feat: correct chhldren area

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,7 @@ const App = () => {
   };
 
   const handleCardClick = (cardLink, cardTitle) => {
-    console.log(cardLink, cardTitle)
+    console.log(cardLink, cardTitle);
     setSelectedCard({ cardLink, cardTitle });
   };
 
@@ -45,89 +45,80 @@ const App = () => {
         name={`edit-avatar`}
         title={`Обновить аватар`}
         button={`Сохранить`}
-        children={
-          <>
-            <input
-              type="url"
-              id="avatarLink"
-              name="link"
-              className="popup__input popup__input_avatar-link"
-              placeholder="Ссылка на аватар"
-              autoComplete="off"
-              required
-            />
-            <span id="avatarLink-error" className="popup__input-error" />
-          </>
-        }
-      />
+      >
+        <input
+          type="url"
+          id="avatarLink"
+          name="link"
+          className="popup__input popup__input_avatar-link"
+          placeholder="Ссылка на аватар"
+          autoComplete="off"
+          required
+        />
+        <span id="avatarLink-error" className="popup__input-error" />
+      </PopupWithForm>
       <PopupWithForm
         isOpen={isEditProfilePopupOpen}
         onClose={() => setIsEditProfilePopupOpen(false)}
         name={`edit-profile`}
         title={`Редактировать профиль`}
         button={`Сохранить`}
-        children={
-          <>
-            <input
-              type="text"
-              id="fullName"
-              className="popup__input popup__input_name-profile"
-              placeholder="Имя Фамилия"
-              autoComplete="off"
-              minLength={2}
-              maxLength={40}
-              defaultValue
-              required
-            />
-            <span id="fullName-error" className="popup__input-error" />
-            <input
-              type="text"
-              id="jobPosition"
-              className="popup__input popup__input_job-profile"
-              placeholder="Профессия или должность"
-              autoComplete="off"
-              minLength={2}
-              maxLength={200}
-              defaultValue
-              required
-            />
-            <span id="jobPosition-error" className="popup__input-error" />
-          </>
-        }
-      />
+      >
+        <input
+          type="text"
+          id="fullName"
+          className="popup__input popup__input_name-profile"
+          placeholder="Имя Фамилия"
+          autoComplete="off"
+          minLength={2}
+          maxLength={40}
+          defaultValue
+          required
+        />
+        <span id="fullName-error" className="popup__input-error" />
+        <input
+          type="text"
+          id="jobPosition"
+          className="popup__input popup__input_job-profile"
+          placeholder="Профессия или должность"
+          autoComplete="off"
+          minLength={2}
+          maxLength={200}
+          defaultValue
+          required
+        />
+        <span id="jobPosition-error" className="popup__input-error" />
+      </PopupWithForm>
       <PopupWithForm
         isOpen={isAddPlacePopupOpen}
         onClose={() => setIsAddPlacePopupOpen(false)}
         name={`add-place`}
         title={`Новое место`}
         button={`Добавить`}
-        children={
-          <>
-            <input
-              type="text"
-              id="placeName"
-              name="name"
-              className="popup__input popup__input_name-place"
-              placeholder="Название"
-              autoComplete="off"
-              minLength={2}
-              maxLength={30}
-              required
-            />
-            <span id="placeName-error" className="popup__input-error" />
-            <input
-              type="url"
-              id="placeLink"
-              name="link"
-              className="popup__input popup__input_link-place"
-              placeholder="Ссылка на картинку"
-              autoComplete="off"
-              required
-            />
-            <span id="placeLink-error" className="popup__input-error" />
-          </>
-        }
-      />
+      >
+        <input
+          type="text"
+          id="placeName"
+          name="name"
+          className="popup__input popup__input_name-place"
+          placeholder="Название"
+          autoComplete="off"
+          minLength={2}
+          maxLength={30}
+          required
+        />
+        <span id="placeName-error" className="popup__input-error" />
+        <input
+          type="url"
+          id="placeLink"
+          name="link"
+          className="popup__input popup__input_link-place"
+          placeholder="Ссылка на картинку"
+          autoComplete="off"
+          required
+        />
+        <span id="placeLink-error" className="popup__input-error" />
+      </PopupWithForm>
       <PopupWithForm
         isOpen={isDelConfirmPopupOpen}
         onClose={() => setIsDelConfirmPopupOpen(false)}
