f3d998d271c045365641d8fd97e375a577814b97
Pavel Khokhlov <ya.pavel-kh@yandex.ru>
2022-04-19 19:09:34 +0300

    bug: fix scroll due popup

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { closeAllPopups } from "../../store/appSlice";
-import { DELAY, ESC_CODE } from "../../utils/config";
+import { ESC_CODE } from "../../utils/config";
 import "./Popup.css";
 
 const Popup = ({ isOpen, children }) => {
   const dispatch = useDispatch();
 
-  const handleEsc = (e) =>
-    e.keyCode === ESC_CODE ? dispatch(closeAllPopups()) : "";
+  const handleEsc = (e) => {
+    if (e.keyCode === ESC_CODE) dispatch(closeAllPopups());
+  };
 
   useEffect(() => {
     if (isOpen) {
       window.addEventListener("keydown", handleEsc);
-      // When the modal is shown, we want a fixed body
-      document.querySelector('.popup').classList.add('show')
       const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
       const body = document.body;
       body.style.position = 'fixed';
@@ -27,9 +26,12 @@ const Popup = ({ isOpen, children }) => {
       body.style.position = '';
       body.style.top = '';
       window.scrollTo(0, parseInt(scrollY || '0') * -1);
-      document.querySelector('.popup').classList.remove('show');
     }
-  }, []);
+  }, [isOpen]);
+
+  window.addEventListener('scroll', () => {
+    document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+  });
 
   const popupClassName = `popup ${isOpen ? "_active" : ""}`;
 
